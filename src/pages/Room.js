5c2591823f3b17fef8959288a518f57081f7607d
petomi <michael.peto10@gmail.com>
2021-05-05 22:03:23 -0400

    wip room page

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../styles.js'
-import { Center } from '@chakra-ui/react'
+import { Center, Stack } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 
 export default function Room (props) {
@@ -10,6 +10,16 @@ export default function Room (props) {
     <Center bg="orange.500" style={styles.pageBackground}>
       Room Code: {roomCode}
       {/* TODO: write out messages here */}
+      <Stack spacing={8}>
+        <Feature
+          title="Plan Money"
+          desc="The future can be even brighter but a goal without a plan is just a wish"
+        />
+        <Feature
+          title="Save Money"
+          desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings on your own terms with our completely automated process"
+        />
+      </Stack>
     </Center>
   )
 }
