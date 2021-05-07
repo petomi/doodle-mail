550619346e1bcbc423714bc3069d1da9f2eee3f2
petomi <michael.peto10@gmail.com>
2021-05-07 18:16:28 -0400

    state working for canvas

--- src/features/draw/Canvas.js
+++ src/features/draw/Canvas.js
@@ -1,54 +1,64 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Stack, Text } from '@chakra-ui/react'
+import { Button, Input, Stack, Text } from '@chakra-ui/react'
 import { sendMessageToRoom } from '../rooms/roomSlice'
 import { success, error } from '../alerts/alertSlice'
-import CanvasDraw from "react-canvas-draw";
-import { withRouter } from "react-router-dom";
+import CanvasDraw from 'react-canvas-draw'
+import { withRouter } from 'react-router-dom'
+import { isMobile } from 'react-device-detect'
 
 
 class Canvas extends Component {
   constructor(props) {
     super(props)
     this.state ={
-      title: 'a',
+      title: 'untitled',
       imageData: '',
-      background: 'a'
+      background: 'white',
+      textFieldFocused: false
     }
   }
 
-  handleSubmit = (evt) => {
-    evt.preventDefault()
+  toggleTextFieldFocus = () => {
+    this.setState({
+      textFieldFocused: !this.state.textFieldFocused
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
     // get image data from canvas
-    const canvasData = this.canvas.getSaveData()
-    this.setState(prevState => ({
-      ...prevState.title,
-      ...prevState.background,
+    let canvasData = this.canvas.getSaveData()
+    console.log('prev state')
+    console.log(this.state)
+    this.setState({
       imageData: canvasData,
-    }))
-    // send message to room
-    const message = {
-      title: this.state.title,
-      imageData: this.state.imageData,
-      background: this.state.background
-    }
-    const sendMessageObject = {
-      roomId: this.props.roomData._id,
-      userName: this.props.userName,
-      messages: [message]
-    }
-    this.props.sendMessageToRoom(sendMessageObject).then((data) => {
-      if (data.type === "room/sendMessage/fulfilled") {
-        this.props.success(`Message sent to room: ${this.props.roomData.entryCode}`)
-        this.props.history.push('/room')
-      } else {
-        this.props.error(`Failed to send message!`)
-        this.setState({
-          title: '',
-          imageData: '',
-          background: ''
-        })
+    }, () => {
+      console.log('after state')
+      console.log(this.state)
+      let sendMessageObject = {
+        roomId: this.props.roomData._id,
+        userName: this.props.userName,
+        messages: [{
+          title: this.state.title,
+          imageData: this.state.imageData,
+          background: this.state.background
+        }]
       }
+      this.props.sendMessageToRoom(sendMessageObject).then((data) => {
+        if (data.type === "room/sendMessage/fulfilled") {
+          this.props.success(`Message sent to room: ${this.props.roomData.entryCode}`)
+          this.props.history.push('/room')
+        } else {
+          this.props.error(`Failed to send message!`)
+          this.setState({
+            title: '',
+            imageData: '',
+            background: '',
+            textFieldFocused: false
+          })
+        }
+      })
     })
   }
 
@@ -56,11 +66,33 @@ class Canvas extends Component {
     if (this.props.roomCode != null) {
       return (
 
-        <Stack spacing={16}>
-          {/* // TODO: add title field with state */}
+        <Stack spacing={8}
+          onKeyPress={(e) => { if (e.key === 'Enter') this.handleSubmit(e) }}
+        >
+          <Input
+            pr="4.5rem"
+            type="text"
+            variant="filled"
+            className="form-field"
+            placeholder="Enter"
+            onFocus={this.toggleTextFieldFocus}
+            onBlur={this.toggleTextFieldFocus}
+            style={{ color: this.state.textFieldFocused ? 'white' : 'black' }}
+            value={this.state.title}
+            onChange={(e) => {
+              this.setState({
+                title: e.target.value
+              })}
+            }
+          ></Input>
+          <CanvasDraw
+            ref={canvasDraw => (this.canvas = canvasDraw)}
+            canvasWidth={isMobile ? 1000 : 600}
+            canvasHeight={isMobile? 1000 : 600}
+          />
+          {/* TODO: add controls for drawing */}
           {/* // TODO: add background field with state */}
-          <CanvasDraw ref={canvasDraw => (this.canvas = canvasDraw)}/>
-          <Button onClick={(evt) => this.handleSubmit(evt)}>Submit</Button>
+          <Button onClick={(e) => this.handleSubmit(e) } style={{color: 'black', width: '50%', alignSelf: 'center'}}>Submit</Button>
           {/* // TODO: create way to save picture to imageData state. See https://github.com/embiem/react-canvas-draw/blob/master/demo/src/index.js */}
         </Stack>
       )
