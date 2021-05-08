936ecdab675c7b21ec1f5b936270aca424e8ddeb
petomi <michael.peto10@gmail.com>
2021-05-08 00:29:05 -0400

    minor mobile fixes

--- src/features/draw/Canvas.js
+++ src/features/draw/Canvas.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Center, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Spacer, Stack, Text } from '@chakra-ui/react'
+import { Button, Center, Container, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Spacer, Stack, Text } from '@chakra-ui/react'
 import { sendMessageToRoom } from '../rooms/roomSlice'
 import { success, error } from '../alerts/alertSlice'
 import CanvasDraw from 'react-canvas-draw'
@@ -77,115 +77,118 @@ class Canvas extends Component {
 
   render() {
     if (this.props.roomCode != null) {
+      // TODO: add mobile styling
       return (
-        <Stack spacing={8}
-          onKeyPress={(e) => { if (e.key === 'Enter') this.handleSubmit(e) }}
-        >
-          <Input
-            pr="4.5rem"
-            type="text"
-            variant="filled"
-            className="form-field"
-            placeholder="Enter"
-            onFocus={this.toggleTextFieldFocus}
-            onBlur={this.toggleTextFieldFocus}
-            style={{ color: this.state.textFieldFocused ? 'white' : 'black' }}
-            value={this.state.title}
-            onChange={(e) => {
-              this.setState({
-                title: e.target.value
-              })}
-            }
-          ></Input>
-          <CanvasDraw
-            ref={canvasDraw => (this.canvas = canvasDraw)}
-            canvasWidth={isMobile ? 1000 : 600}
-            canvasHeight={isMobile? 1000 : 600}
-            backgroundColor={this.state.backgroundColor}
-            brushColor={this.state.brushColor}
-            brushRadius={this.state.brushRadius}
-          />
-          <Stack spacing={4} direction="row" align="center">
-            <IconButton
-              variant="outline"
-              colorScheme="black"
-              aria-label="Increase brush size"
-              icon={<FaPlus/>}
-              onClick={() => this.setState({
-                brushRadius: this.state.brushRadius + 4
-              })}
+        <Container>
+          <Stack spacing={8}
+            onKeyPress={(e) => { if (e.key === 'Enter') this.handleSubmit(e) }}
+          >
+            <Input
+              pr="4.5rem"
+              type="text"
+              variant="filled"
+              className="form-field"
+              placeholder="Enter"
+              onFocus={this.toggleTextFieldFocus}
+              onBlur={this.toggleTextFieldFocus}
+              style={{ color: this.state.textFieldFocused ? 'white' : 'black' }}
+              value={this.state.title}
+              onChange={(e) => {
+                this.setState({
+                  title: e.target.value
+                })}
+              }
+            ></Input>
+            <CanvasDraw
+              ref={canvasDraw => (this.canvas = canvasDraw)}
+              canvasWidth={isMobile ? 1000 : 280}
+              canvasHeight={isMobile? 1000 : 280}
+              backgroundColor={this.state.backgroundColor}
+              brushColor={this.state.brushColor}
+              brushRadius={this.state.brushRadius}
             />
-            <IconButton
-              variant="outline"
-              colorScheme="black"
-              aria-label="Decrease brush size"
-              icon={<FaMinus/>}
-              onClick={() => this.setState({
-                brushRadius: (this.state.brushRadius === 4) ? 4 : (this.state.brushRadius - 4)
-              })}
-            />
-            <Spacer/>
-            <Text fontSize="lg">Brush:</Text>
-            <IconButton
-              style={{ backgroundColor: this.state.brushColor }}
-              aria-label="Show brush color picker"
-              onClick={() => this.setState({
-                displayColorPicker: true,
-                colorPickerMode: 'brush'
-              })}
-            />
-            <Text fontSize="lg">Background:</Text>
-            <IconButton
-              style={{ backgroundColor: this.state.backgroundColor }}
-              aria-label="Show background color picker"
-              onClick={() => this.setState({
-                displayColorPicker: true,
-                colorPickerMode: 'background'
-              })}
-            />
-            <Modal isOpen={this.state.displayColorPicker} onClose={() => this.setState({ displayColorPicker: false })} isCentered={true} size="xs">
-              <ModalOverlay/>
-              <ModalContent style={{background: 'none', boxShadow: 'none'}}>
-                <ModalCloseButton/>
-                <ModalBody>
-                  <Center>
-                    <ChromePicker
-                      color={ this.state.colorPickerMode === 'background' ? this.state.backgroundColor : this.state.brushColor }
-                      onChangeComplete={ (color) => {
-                        if (this.state.colorPickerMode === 'background') {
-                          this.setState({
-                            backgroundColor: color.hex
-                          })
-                        } else {
-                          this.setState({
-                            brushColor: color.hex
-                          })
-                        }
-                      }}
-                      width="85%"
-                    />
-                  </Center>
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-            <Spacer/>
-            <IconButton
-              variant="outline"
-              colorScheme="black"
-              aria-label="Undo last stroke"
-              icon={<FaUndo/>}
-              onClick={() => this.canvas.undo()}
-            />
-            <IconButton
-              variant="outline"
-              colorScheme="black"
-              aria-label="Erase picture"
-              icon={<FaEraser/>}
-              onClick={() => this.canvas.clear()}
-            />
+            <Stack spacing={4} direction="row" align="center">
+              <IconButton
+                variant="outline"
+                colorScheme="black"
+                aria-label="Increase brush size"
+                icon={<FaPlus/>}
+                onClick={() => this.setState({
+                  brushRadius: this.state.brushRadius + 4
+                })}
+              />
+              <IconButton
+                variant="outline"
+                colorScheme="black"
+                aria-label="Decrease brush size"
+                icon={<FaMinus/>}
+                onClick={() => this.setState({
+                  brushRadius: (this.state.brushRadius === 4) ? 4 : (this.state.brushRadius - 4)
+                })}
+              />
+              <Spacer/>
+              <Text fontSize="lg">Brush:</Text>
+              <IconButton
+                style={{ backgroundColor: this.state.brushColor }}
+                aria-label="Show brush color picker"
+                onClick={() => this.setState({
+                  displayColorPicker: true,
+                  colorPickerMode: 'brush'
+                })}
+              />
+              <Text fontSize="lg">Background:</Text>
+              <IconButton
+                style={{ backgroundColor: this.state.backgroundColor }}
+                aria-label="Show background color picker"
+                onClick={() => this.setState({
+                  displayColorPicker: true,
+                  colorPickerMode: 'background'
+                })}
+              />
+              <Modal isOpen={this.state.displayColorPicker} onClose={() => this.setState({ displayColorPicker: false })} isCentered={true} size="xs">
+                <ModalOverlay/>
+                <ModalContent style={{background: 'none', boxShadow: 'none'}}>
+                  <ModalCloseButton/>
+                  <ModalBody>
+                    <Center>
+                      <ChromePicker
+                        color={ this.state.colorPickerMode === 'background' ? this.state.backgroundColor : this.state.brushColor }
+                        onChangeComplete={ (color) => {
+                          if (this.state.colorPickerMode === 'background') {
+                            this.setState({
+                              backgroundColor: color.hex
+                            })
+                          } else {
+                            this.setState({
+                              brushColor: color.hex
+                            })
+                          }
+                        }}
+                        width="85%"
+                      />
+                    </Center>
+                  </ModalBody>
+                </ModalContent>
+              </Modal>
+              <Spacer/>
+              <IconButton
+                variant="outline"
+                colorScheme="black"
+                aria-label="Undo last stroke"
+                icon={<FaUndo/>}
+                onClick={() => this.canvas.undo()}
+              />
+              <IconButton
+                variant="outline"
+                colorScheme="black"
+                aria-label="Erase picture"
+                icon={<FaEraser/>}
+                onClick={() => this.canvas.clear()}
+              />
+            </Stack>
+            <Button onClick={(e) => this.handleSubmit(e) } style={{color: 'black', width: '50%', alignSelf: 'center'}}>Submit</Button>
           </Stack>
-          <Button onClick={(e) => this.handleSubmit(e) } style={{color: 'black', width: '50%', alignSelf: 'center'}}>Submit</Button>
-        </Stack>
+        </Container>
       )
     }
     else {
