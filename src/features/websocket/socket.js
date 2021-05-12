d928f277bdef470f5a084b27638204b600a90b31
petomi <michael.peto10@gmail.com>
2021-05-12 00:03:55 -0400

    get socket URL from env variables

--- src/features/websocket/socket.js
+++ src/features/websocket/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:5000' // TODO - get this from .env
+const URL = process.env.BASE_API_URL || 'http://localhost:5000'
 const socket = io(URL, { autoConnect: false })
 
 // TODO: remove after debug
